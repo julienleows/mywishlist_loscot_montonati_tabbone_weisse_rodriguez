3a6a29e9f36104dedd7821d81a50ec88c9a92105
Lucas Tabbone <lucas.tabbone12@gmail.com>
2020-12-14 18:03:15 +0100

    ajout du squelette de la classe controleurdesitems

--- src/controleurs/GestionDesItems.php
+++ src/controleurs/GestionDesItems.php
@@ -7,3 +7,42 @@
  * @author: Clement LOSCOT
  * @author: Godefroy MONTONATI
  */
+
+namespace mywishlist\controls;
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+class GestionDesItems {
+
+  private $c ;
+
+  /**
+  * Constructeur vide de la classe GestionDesItems : pour l'instant 
+  **/
+  public function __construct(\Slim\Container $c) {
+      $this->c=$c;
+  }
+
+  /**
+  *Fct 8 : Ajouter un item
+  **/
+  function ajouterItem(Request $rq,Response $rs,array $args) : Response{
+
+  }
+
+  /**
+  *Fct 9 : Modification d'un item
+  **/
+  function modifierItem(Request $rq,Response $rs,array $args) : Response {
+
+  }
+
+  /**
+  *Fct 9 : Suppression d'un item
+  **/
+  function supprimerItem(Request $rq,Response $rs,array $args) : Response {
+
+  }
+
+}
