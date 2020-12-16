422695acae29f6817742faf398eefb48e1787880
Clement Loscot <clement.loscot8@etu.univ-lorraine.fr>
2020-12-16 11:15:56 +0100

    commitControleurs

--- src/controls/GestionDesImages.php
+++ src/controls/GestionDesImages.php
@@ -1,9 +1,50 @@
 <?php
 /**
- * Fichier: ....
- * description: ....
+ * Fichier: GestionDesImages
+ * description: le controleur de gestion de toutes les images
  * @author: Julien WEISSE
  * @author: Lucas TABBONE
  * @author: Clement LOSCOT
  * @author: Godefroy MONTONATI
  */
+
+ namespace mywishlist\controls;
+
+ use \Psr\Http\Message\ServerRequestInterface as Request;
+ use \Psr\Http\Message\ResponseInterface as Response;
+
+ class GestionDesImages {
+
+   private $app;
+
+   /**
+    * constructeur de la classe GestionDesImages
+    **/
+   public function __construct($app) {
+       $this->app = $app;
+   }
+
+   /* fct 11 : ajouter/rajouter une image a un item */
+   function ajouterImage(Request $rq, Response $rs, $args){
+       $rs->getBody()->write("Ajouter une image a un item");
+       return $rs;
+   }
+
+   /* fct 12 : modifier une image d'un item */
+   function modifierImage(Request $rq, Response $rs, $args){
+       $rs->getBody()->write("Modifier une image d'un item");
+       return $rs;
+   }
+
+   /* fct 13 : supprimer une image d'un item */
+   function supprimerImage(Request $rq, Response $rs, $args){
+       $rs->getBody()->write("Supprimer l'image d'un item");
+       return $rs;
+   }
+
+   /* fct 24 : uploader une image */
+   function UploaderImage(Request $rq, Response $rs, $args){
+       $rs->getBody()->write("Mettre en ligne une nouvelle image");
+       return $rs;
+   }
+}
--- src/controls/ParticipationListe.php
+++ src/controls/ParticipationListe.php
@@ -1,9 +1,68 @@
 <?php
 /**
- * Fichier: ....
- * description: ....
+ * Fichier: ParticipationListe
+ * description: le controleur de gestion de la participation a une liste
  * @author: Julien WEISSE
  * @author: Lucas TABBONE
  * @author: Clement LOSCOT
  * @author: Godefroy MONTONATI
  */
+
+namespace mywishlist\controls;
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+class ParticipationListe {
+
+  private $app;
+
+  /**
+   * constructeur de la classe ParticipationListe
+   **/
+  public function __construct($app) {
+      $this->app = $app;
+  }
+
+  /* fct 1 : Afficher une liste de souhaits */
+  function afficherListeSouhaits(Request $rq, Response $rs, $args){
+      $rs->getBody()->write("Affichage de la liste de souhaits");
+      return $rs;
+  }
+
+  /* fct 2 : Afficher un item d'une liste de souhaits */
+  function afficherItemListeSouhaits(Request $rq, Response $rs, $args){
+      $rs->getBody()->write("Affichage d'un item de la liste de souhaits");
+      return $rs;
+  }
+
+  /* fct 3 : Reserver un item */
+  function reserverItem(Request $rq, Response $rs, $args){
+      $rs->getBody()->write("Reserver un item");
+      return $rs;
+  }
+
+  /* fct 14 : partager une liste */
+  function partagerListe(Request $rq, Response $rs, $args){
+      $rs->getBody()->write("Partager la liste");
+      return $rs;
+  }
+
+  /* fct 15 : Consulter les réservations d'une de ses listes avant échéance */
+  function consulterReservationListeAvantEcheance(Request $rq, Response $rs, $args){
+      $rs->getBody()->write("Consultation des reservations de la liste avant l'échéance");
+      return $rs;
+  }
+
+  /* fct 22 : Creer une cagnotte sur un item */
+  function creerCagnotteSurItem(Request $rq, Response $rs, $args){
+      $rs->getBody()->write("creation d'une cagnotte sur un item");
+      return $rs;
+  }
+
+  /* fct 23 : Participer a une cagnotte */
+  function creerCagnotteSurItem(Request $rq, Response $rs, $args){
+      $rs->getBody()->write("participation a la cagnotte d'un item");
+      return $rs;
+  }
+}
