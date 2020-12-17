779bafca6de4c18993cf063f80b0562491a757e2
Lucas Tabbone <lucas.tabbone12@gmail.com>
2020-12-17 17:38:47 +0100

    ajout des méthodes de la classe VueParticipant à compléter

--- src/view/VueParticipant.php
+++ src/view/VueParticipant.php
@@ -3,8 +3,40 @@
 
 namespace mywishlist\view;
 
+use mywishlist\models\item as item;
+use mywishlist\models\liste as liste;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
 class VueParticipant
 {
+    private $tab;
+
+    public function __construct(array $t) {
+        $this->tab=$t;
+    }
+
+    private function affichageListe(array $list) {
+
+    }
+
+    private function affichageElementsListe(array $items,liste $ls) : string {
+
+    }
+
+    private function affichageItem(item $item) : string {
+        $html=<<<END
+        <section class="content">
+        <h1>Nom : ($item->nom)</h1>
+        <p> Desciption : ($item->descr)</p>
+        <h3>Tarif : ($item->tarif)</h3>
+</section>  
+END;
+    return $html;
+    }
+
+    public function render(array $vars) {
+
+    }
 
 }
