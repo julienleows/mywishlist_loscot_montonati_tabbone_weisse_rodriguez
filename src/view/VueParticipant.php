0b3727c4046dc5e7657fa349a251d1c482409d1d
Lucas Tabbone <lucas.tabbone12@gmail.com>
2020-12-18 16:02:50 +0100

    ajout de méthodes dans la classe VueParticipant

--- src/view/VueParticipant.php
+++ src/view/VueParticipant.php
@@ -9,18 +9,42 @@ class VueParticipant
 {
     private $tab;
 
+    /**
+     * Constructeur de la VueParticipant
+     * @param array $t modèle
+     */
     public function __construct(array $t) {
         $this->tab=$t;
     }
 
+    /**
+     * Afficha de la liste des listes publiques
+     * @param array $list
+     */
     private function affichageListe(array $list) {
 
     }
 
+    /**
+     * Affichage de l'ensemble des élements d'une liste
+     * @param array $items ensemble des items à afficher
+     * @param liste $ls liste dont on souhaite afficher les items
+     * @return string
+     */
     private function affichageElementsListe(array $items,liste $ls) : string {
-
+        $html="<div><ul>";
+        foreach($items as $it) {
+            $html .="<li>".$it->nom."</li>";
+        }
+        $html .="</ul></div>";
+        return $html;
     }
 
+    /**
+     * Affichage d'un item
+     * @param item $item item qu'on souhaite afficher
+     * @return string
+     */
     private function affichageItem(item $item) : string {
         $html=<<<END
         <section class="content">
@@ -32,7 +56,12 @@ END;
     return $html;
     }
 
-    public function render(array $vars) {
+    /**
+     * Méthode pour choisir l'affichage désiré
+     * @param array $vars
+     * @param $selecteur
+     */
+    public function render(array $vars,$selecteur) {
 
     }
 
