3c9edab795ded8c8c805bbc34ec52ed8c1d35a57
julienws <julienleo.weisse@gmail.com>
2020-12-21 14:27:54 +0100

    mises a jour index.php + connection database

--- index.php
+++ index.php
@@ -21,41 +21,25 @@ use \mywishlist\controls\ControleurDesMessages as controleurMessage;
 use \mywishlist\controls\ControleurDesImages as controleurImage;
 use \mywishlist\controls\ControleurParticipationListe as controleurParticipation;
 
-# affichage des erreurs systeme de Slim
-$config = ['settings' => ['displayErrorDetails' => true]];
-
-# connection base de donnees MySQL
-$db = new \Illuminate\Database\Capsule\Manager();
-$db->addConnection(parse_ini_file('src/conf/conf.ini'));
-$db->setAsGlobal();
-$db->bootEloquent();
+# affichage des erreurs systeme de Slim et connection database
+$config = ['settings' => [
+        'displayErrorDetails' => true,
+        'dbconf' => '/conf/conf.ini',
+]];
 
 # creation instance App Slim
 $container = new \Slim\Container($config);
 $app = new \Slim\App($container);
 
 # ======= ROUTES =======
-# affichage de la liste des listes de souhaits
+# fct 21 : afficher les listes de souhaits qui sont en publiques
 $app->get('/listes[/]', function (Request $rq, Response $rs, array $args) use ($container): Response {
     $ctrl = new ControleurDesListes($container);
     return $ctrl->afficherListePublique($rq, $rs, $args);
 }
 );
 
-# affichage de la liste des items d'une liste de souhaits
-$app->get('/items/{id}[/]', function (Request $rq, Response $rs, array $args): Response {
-    $c= new controleurParticipation($this);
-    return $c->afficherListeSouhaits($rq,$rs,$args);
-}
-);
-
-# affichage d'un item désignée par son ID
-$app->get('/item/{id}[/]', function (Request $rq, Response $rs, array $args): Response {
-    $id = $args['id'];
-    $rs->getBody()->Write("<h1>affichage d'un item désignée par son ID : $id</h1>");
-    return $rs;
-}
-);
+# ...
 
 # declenchement du traitement de la requette HTTP courante par le framework Slim
 $app->run();
